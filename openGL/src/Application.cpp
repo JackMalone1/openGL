415631281b685adbd980ca13b2ee90e1b8b77815
Jack Malone <c00236428@itcarlow.ie>
2019-05-15 21:16:25 +0100

    drawing a rectangle on the screen using two triangles and an index buffering system to avoid duplication of vertices in memory

--- openGL/src/Application.cpp
+++ openGL/src/Application.cpp
@@ -1,6 +1,53 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct programShaderSource
+{
+	std::string vertexSource;
+	std::string fragmantSource;
+};
+
+static programShaderSource parseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType {
+		None = -1,
+		Vertex = 0,
+		Fragment = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::None;
+
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				type = ShaderType::Vertex;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				type = ShaderType::Fragment;
+			}
+		}
+		else 
+		{
+			ss[(int)type] << line << "\n";
+		}
+	}
+
+	return { ss[0].str(),ss[1].str() };
+}
 
 static unsigned int compileShader(const std::string& source,
 								  unsigned int type)
@@ -24,7 +71,7 @@ static unsigned int compileShader(const std::string& source,
 
 		glGetShaderInfoLog(id, length, &length, message);
 
-		std::cout << "Failed to copmpile " <<
+		std::cout << "Failed to compile " <<
 			(type == GL_VERTEX_SHADER ? "vertex" : "fragment" ) <<
 			"shader!" << std::endl;
 		std::cout << message << std::endl;
@@ -83,42 +130,34 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float positions[6] = { 
+	float positions[] = { 
 		-0.5f,-0.5f,
-		0.0f,0.5f,
-		0.5f,-0.5f
+		0.5f,-0.5f,
+		0.5f,0.5f,
+		-0.5f,0.5f,
+	};
+
+	unsigned int indices[] = {
+		0,1,2,
+		2,3,0
 	};
 
 	unsigned int buffer;
 	glGenBuffers(1,&buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+	unsigned int indexedBufferObject;
+	glGenBuffers(1, &indexedBufferObject);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexedBufferObject);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 *  sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-	std::string vertexShader = 
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"\n"
-		"void main()"
-		"{\n"
-		"	gl_Position = position;"
-		"}\n";
+	programShaderSource source = parseShader("res/shaders/Basic.shader");
 
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;\n"
-		"\n"
-		"void main()"
-		"{\n"
-		"	color = vec4(1.0,0.0,0.0,1.0);\n"
-		"}\n";
-
-	unsigned int shader = createShader(vertexShader,fragmentShader);
+	unsigned int shader = createShader(source.vertexSource,source.fragmantSource);
 	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
@@ -127,7 +166,7 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES,0,3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
